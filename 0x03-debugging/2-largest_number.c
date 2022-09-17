42773a8f2cd14f4c190739e0cecc8055f10d5c26
Nsikak Uwah <uwahnsikakusen@gmail.com>
2022-09-16 20:45:06 -0700

    2-largest_number.c

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * larget_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int larget_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
+
+
