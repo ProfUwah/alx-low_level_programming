c1adf3ab1cd9c13a749726a1c76f3bb57d3071a7
Nsikak Uwah <uwahnsikakusen@gmail.com>
2022-09-19 17:21:28 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,10 +11,11 @@ void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*(str + i) != '\0')
 	{
-		putchar(str[i]);
+		putchar(*(str + i));
+		i++;
 	}
-	putchar('\n');
 }
+
 
