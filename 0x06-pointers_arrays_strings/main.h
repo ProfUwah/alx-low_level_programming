85387fca1f5b76024dc79260f7f9882219f7cd9f
Nsikak Uwah <uwahnsikakusen@gmail.com>
2022-09-22 11:44:46 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -18,6 +18,8 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *add_strings(char *n1, char *n2, char*r, int r_index);
+int _putchar(char);
+
 
 #endif /*MAIN_H*/
 
