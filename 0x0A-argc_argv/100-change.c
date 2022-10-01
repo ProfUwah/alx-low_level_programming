657bba963cb92c7c3efc4b7e128592f8c023fbeb
Nsikak Uwah <uwahnsikakusen@gmail.com>
2022-09-30 20:45:13 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,52 +1,44 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * main - Program that prints the minimum number of coins to make
- * change for an amount of money.
- * @argc: count
- * @argv: array
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Always 0
- **/
+ * Return: 0 (Success), 1 (Error)
+ */
 int main(int argc, char *argv[])
 {
-	int values[] = {25, 10, 5, 2, 1}, i, j, result = 0, n;
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc == 2)
-	{
-		n = atoi(argv[1]);
-		for (i = 0; i < 5; i++)
-		{
-			if (n < 0)
-			{
-				printf("0");
-				return (0);
-			}
-			if (n == 0)
-			{
-				printf("%d\n", result);
-				return (0);
-			}
-			if (values[i] <= n)
-			{
-				result += n / values[i];
-				n = abs(n - (values[i] * result));
-			}
-			for (j = 0; j < 5; j++)
-			{
-				if (n == values[j])
-				{
-					result += 1;
-					n = 0;
-				}
-			}
-		}
-		printf("%d\n", result);
-	} else
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+
+	printf("%d\n", result);
 	return (0);
 }
